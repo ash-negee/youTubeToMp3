a85aa97a10a8dce06d70d1760d95eaefa09e984e
Ash <ashunegincr@gmail.com>
2025-01-05 14:18:41 +0530

    Added Buy me coffee widget.

--- src/components/YouTubeToMp3.jsx
+++ src/components/YouTubeToMp3.jsx
@@ -231,6 +231,7 @@ function YouTubeConverter() {
 
 						<Card.Footer className="bg-white text-center text-muted small">
 							<p className="mb-0">Made with 🤍 by - Ash</p>
+							<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="ashunegi" data-color="#FFDD00" data-emoji="" data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
 						</Card.Footer>
 					</Card>
 				</Col>
